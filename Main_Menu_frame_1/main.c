a65fa6e05b52c2f5d5dc055a3627a613624aa167
Alois Taranetz <alois.taranetz@gmx.at>
2011-04-26 01:01:25 +0200

    status bar working, the most menu points are in

--- Main_Menu_frame_1/main.c
+++ Main_Menu_frame_1/main.c
@@ -1,7 +1,8 @@
 // A program
 // which displays the main window with full menus and working statusbar
 //24.04.2011 V 0.0 created by AT
-//25.04.2011
+//25.04.2011-26.04.2011 Build Statusbar Messages working correct
+
 /*#################################*/
 /* Libaries */
 /*#################################*/
@@ -13,9 +14,14 @@
 /*Function Prototypes*/
 /*#################################*/
 GdkPixbuf *create_pixbuf(const gchar * filename);
+GtkWidget *statusbar;
 void toggle_statusbar(GtkWidget *widget, gpointer statusbar);
 void toggle_toolbar(GtkWidget *widget, gpointer toolbar);
 void show_about(GtkWidget *widget, gpointer data);
+void file_open(GtkWidget *widget, gpointer data);
+void push_item( GtkWidget *widget,gpointer data );
+void pop_item( GtkWidget *widget,gpointer data );
+static gboolean statusbar_hint (GtkMenuItem *menuitem,GdkEventProximity *event,GtkStatusbar *statusbar);    
 /*#################################*/
 /*Global Variables*/
 /*#################################*/
@@ -24,7 +30,7 @@ int main( int argc, char *argv[])
   /* Create the Widgets */
   GtkWidget *window, *vbox, *menubar, *filemenu,*editmenu,*viewmenu,*helpmenu,
   *file, *edit, *view, *help,*pref_1,*toolbar,*help_Dialog,*about_Dialog,
-  *new, *open, *quit, *sep, *tog_stat, *tog_tool, *statusbar;
+  *new,*sep1,*sep2, *open,*save, *save_as,*export, *quit, *sep, *tog_stat, *tog_tool;
   
   /* Some nesseary inits */
   GtkAccelGroup *accel_group = NULL;
@@ -73,32 +79,107 @@ int main( int argc, char *argv[])
   /* Create the Help Drop Down Point in the menu bar with mnemonic character */
   help = gtk_menu_item_new_with_mnemonic("_Help");
   /* ############################# Items for the Menu Bar################## */
-  /* Use the New and the open point from the libary and their stock symbols */
-  new = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, NULL);
+  /* Use the open point from the libary and the stock symbol */
   open = gtk_image_menu_item_new_from_stock(GTK_STOCK_OPEN, NULL);
+  /*Connect signal to display the Message for the Save Menu Item in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (open), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (open), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (open), "menuhint",(gpointer) "Open a recorded File");
+  /* Create vertical seperator line in the file menu */
+  sep1 = gtk_separator_menu_item_new();
+  /* Use the save point from the libary and the stock symbol */
+  save = gtk_image_menu_item_new_from_stock(GTK_STOCK_SAVE, NULL);
+  /*Connect signal to display the Message for the Save Menu Item in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (save), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (save), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (save), "menuhint",(gpointer) "Save to specified File");
+  /* Use the save_as point from the libary and the stock symbol */
+  save_as = gtk_image_menu_item_new_from_stock(GTK_STOCK_SAVE_AS, NULL);
+  /*Connect signal to display the Message for the Save_as Menu Item in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (save_as), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (save_as), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (save_as), "menuhint",(gpointer) "Save to a new File");
+  /* Create vertical seperator line in the file menu */
+  sep2 = gtk_separator_menu_item_new();
+  /* For the export function, no stock symbol exists, used instead a point with only a label*/
+  export = gtk_menu_item_new_with_label("Export");
+  /*Connect signal to display the Message for the Export Menu Item in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (export), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (export), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (export), "menuhint",(gpointer) "Export Recorded File");
   /* Create vertical seperator line in the file menu */
   sep = gtk_separator_menu_item_new();
   quit = gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT, accel_group);
+  /*Connect signal to display the Message for the Quit Menu Item in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (quit), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (quit), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (quit), "menuhint",(gpointer) "End Program");
   /* Add accelerator to quit point in the file menu */
   gtk_widget_add_accelerator(quit, "activate", accel_group,GDK_q, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE); 
   /* ############################# Items for the Edit Bar################## */
   pref_1= gtk_menu_item_new_with_label("Preferences");
+  /*Connect signal to display the Message for the Preferneces Menu Item in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (pref_1), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (pref_1), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (pref_1), "menuhint",(gpointer) "Show the Preferences Tab");
   /* ############################# Items for the View Bar################## */
+  /*Menu Item for Show Hide the Statusbarbar*/
   tog_stat = gtk_check_menu_item_new_with_label("View Statusbar");
   gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(tog_stat), TRUE);
+  /*Connect signal to display the Message for the show/hide Statusbar Menu Item in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (tog_stat), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (tog_stat), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (tog_stat), "menuhint",(gpointer) "Show/Hide the Statusbarbar");
+  /*Menu Item for Show Hide the Toolbar*/
   tog_tool = gtk_check_menu_item_new_with_label("View Toolbar");
   gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(tog_tool), TRUE);
-  
-  
+  /*Connect signal to display the Message for the show/hide Toolbar Menu Item in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (tog_tool), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (tog_tool), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (tog_tool), "menuhint",(gpointer) "Show/Hide the Toolbar");
   /* ############################# Items for the Help Bar################## */
   help_Dialog= gtk_menu_item_new_with_label("Get Help Online");
+  /*Connect signal to display the Message for the help dialog in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (help_Dialog), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves this menu point*/ 
+  g_signal_connect (G_OBJECT (help_Dialog), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (help_Dialog), "menuhint",(gpointer) "Get Online Help");
+
   about_Dialog= gtk_menu_item_new_with_label("About");
   /*Connect signal to view the about dialog*/
   g_signal_connect(G_OBJECT(about_Dialog), "activate",G_CALLBACK(show_about),show_about);
+  /*Connect signal to display the Message for the about dialog in the statusbar, when the mouse cursors hovers over*/
+  g_signal_connect (G_OBJECT (about_Dialog), "enter-notify-event",G_CALLBACK (statusbar_hint), statusbar);
+  /*Remove Message from the statusbar when the mouse cursors leaves the about menu point*/ 
+  g_signal_connect (G_OBJECT (about_Dialog), "leave-notify-event",G_CALLBACK (statusbar_hint), statusbar); 
+  /*Attach the text for the statusbar to the Menu Item*/
+  g_object_set_data (G_OBJECT (about_Dialog), "menuhint",(gpointer) "Shows the about Dialog of this Program");
+
   /* Add the File menu and the point content points to the menu bar */
   gtk_menu_item_set_submenu(GTK_MENU_ITEM(file), filemenu);
-  gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), new);
   gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), open);
+  gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), sep1);
+  gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), save);
+  gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), save_as);
+  gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), sep2);
+  gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), export);
   gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), sep);
   gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), quit);
   gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file);
@@ -131,8 +212,9 @@ int main( int argc, char *argv[])
   
   /*Connect signal to view / hide the statusbar*/
   g_signal_connect(G_OBJECT(tog_stat), "activate",G_CALLBACK(toggle_statusbar), statusbar);
-    /*Connect signal to view / hide the toolbar*/
+  /*Connect signal to view / hide the toolbar*/
   g_signal_connect(G_OBJECT(tog_tool), "activate",G_CALLBACK(toggle_toolbar), toolbar);
+  /*Connect signal for the preferences Dialog*/
  // g_signal_connect(G_OBJECT(pref_1), "activate",G_CALLBACK(NULL), NULL);
   /* necessary for the (x) Button in the right corner */
   g_signal_connect_swapped(G_OBJECT(window), "destroy",G_CALLBACK(gtk_main_quit), NULL);
@@ -203,4 +285,55 @@ void show_about(GtkWidget *widget, gpointer data)
   gtk_dialog_run(GTK_DIALOG (dialog));
   gtk_widget_destroy(dialog);
 }
+/*######################################*/
+/*Statusbar Messages*/
+/*######################################*/
+/*Display Message on the Statusbar*/
+/*
+/* Add or remove a status bar menu hint, depending on whether this function
+* is initialized by a proximity-in-event or proximity-out-event. */
+static gboolean statusbar_hint (GtkMenuItem *menuitem,GdkEventProximity *event,GtkStatusbar *statusbar)
+{
+  gchar *hint;
+  guint id = gtk_statusbar_get_context_id (statusbar, "MenuItemHints");
+if (event->type == GDK_ENTER_NOTIFY)
+{
+  hint = (gchar*) g_object_get_data (G_OBJECT (menuitem), "menuhint");
+  gtk_statusbar_push (statusbar, id, hint);
+}
+else if (event->type == GDK_LEAVE_NOTIFY)
+  gtk_statusbar_pop (statusbar, id);
+return FALSE;
+}
+
+ 
+/*######################################*/
+/*File Open Dialog*/
+/*######################################*/
+/*
+void file_open(GtkWidget *widget, gpointer data)
+{
+GtkWidget *dialog;
+dialog = gtk_file_chooser_dialog_new ("Open File",
+				      parent_window,
+				      GTK_FILE_CHOOSER_ACTION_OPEN,
+				      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+				      GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+				      NULL);
+if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT)
+  {
+    char *filename;
+    filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
+    open_file (filename);
+    g_free (filename);
+  }
+gtk_widget_destroy (dialog);
+
+}
+*/
+
+
+
+
+
 
